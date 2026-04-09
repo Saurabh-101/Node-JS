4a92c7f1e7899efcd87118d5a0c45f5afbfbec00
Saurabh Gupta <saurabhgupta72dh3@gmail.com>
2026-04-09 15:43:49 +0530

    Read and Write using util.promisify, async and await

--- app.js
+++ app.js
@@ -1,30 +1,38 @@
 const fs = require('fs');
+const util = require('util');
+// const getText = (path)=>{
+//     // Create a promise to fetch the data using readFile
+//     const promise = new Promise((resolve,reject)=>{
+//         fs.readFile(path,'utf8',(err,data)=>{
+//             if(err){
+//                 reject(err);
+//             }else{
+//                 resolve(data);
+//             }
 
-const getText = (path)=>{
-    // Create a promise to fetch the data using readFile
-    const promise = new Promise((resolve,reject)=>{
-        fs.readFile(path,'utf8',(err,data)=>{
-            if(err){
-                reject(err);
-            }else{
-                resolve(data);
-            }
-
-        })
-    })
-    // return the promise 
-    return promise;
-};
+//         })
+//     })
+//     // return the promise 
+//     return promise;
+// };
 
 // getText returns a promise, thus handle using .then().catch().finally()
 // getText('./first.txt').then((resolve)=>{console.log(resolve)}).catch((error)=>{console.log(error)})
 
+const readFilePromise = util.promisify(fs.readFile);
+const writeFilePromise = util.promisify(fs.writeFile);
+
 const start = async()=>{
 
     try{
-        const first = await getText('./first.txt');
-        const second = await getText('./second.txt');
-    
+        // const first = await getText('./first.txt');
+        // const second = await getText('./second.txt');
+        
+
+        const first = await readFilePromise('./first.txt','utf8');
+        const second = await readFilePromise('./second.txt','utf8');
+
+        await writeFilePromise('./writtenByCode.txt',`Content of file first: ${first} \nAnd content of file second: ${second}`);
         console.log(first,second);
 
     }catch(err){
