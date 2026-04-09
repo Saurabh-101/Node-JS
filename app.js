f3a7427cd36a2a72c17e32bb7afec87d90bd53a3
Saurabh Gupta <saurabhgupta72dh3@gmail.com>
2026-04-09 17:05:07 +0530

    EventEmitter custom events

--- app.js
+++ app.js
@@ -2,10 +2,17 @@ const EventEmitter = require('events');
 
 const customEmitter = new EventEmitter();
 
-customEmitter.on('customEventName',()=>{
+customEmitter.on('customEventName',(param1,param2)=>{
 
-    console.log('Printing log from custom Event');
+    console.log(`Printing log from custom Event and Param1 is :${param1} & Param2 is :${param2}`);
 
 });
 
-customEmitter.emit('customEventName');
+customEmitter.on('customEventName',()=>{
+
+    console.log('Some Other logic on same event name');
+
+});
+
+
+customEmitter.emit('customEventName','Data one','Data two');
