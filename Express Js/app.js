d0045caa4233d766be417304913041cd2d2a4dd7
Saurabh Gupta <saurabhgupta72dh3@gmail.com>
2026-04-17 10:25:46 +0530

    Removed node_modules

---
+++ Express Js/app.js
@@ -0,0 +1,13 @@
+const http = require('http');
+
+const server = http.createServer((req,res)=>{
+
+    res.writeHead(200,{'content-type':'text/html'});
+
+    res.write('<h1>Hello World</h1>');
+
+    res.end();
+
+})
+
+server.listen(5000,()=>{console.log(`server running on PORT 5000`)});
