3e715022dd21321b11d6b59b470d5c535f24eb47
Saurabh Gupta <saurabhgupta72dh3@gmail.com>
2026-04-17 12:43:06 +0530

    Server setup, different paths access different html pages

--- Express Js/app.js
+++ Express Js/app.js
@@ -1,12 +1,21 @@
 const http = require('http');
 
+const fs = require('fs');
+
+const homePage = fs.readFileSync('./index.html');
+const errorPage = fs.readFileSync('./error-page.html');
 const server = http.createServer((req,res)=>{
 
-    res.writeHead(200,{'content-type':'text/html'});
-
-    res.write('<h1>Hello World</h1>');
-
-    res.end();
+    const url = req.url;
+    if(url == '/'){
+        res.writeHead(200,{'content-type':'text/html'});
+        res.write(homePage);
+        res.end();
+    }else{
+        res.writeHead(404,{'content-type':'text/html'});
+        res.write(errorPage);
+        res.end();
+    }
 
 })
 
