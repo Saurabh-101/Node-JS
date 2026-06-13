ed406c1be76805766f3d0f5bac53c6b6379af1b7
Saurabh-101 <saurabhgupta72dh3@gmail.com>
2026-06-13 14:47:21 +0530

    Postman test 01

--- Express Js/app.js
+++ Express Js/app.js
@@ -1,36 +1,20 @@
 const express = require('express');
 const app = express();
-const morgan = require('morgan');
 
-// req => middleware => res
-// middleware import
-const logger = require('./Middlewares/logger');
-const authorize = require('./Middlewares/authorize');
 
-//app.use(logger); // if no path specified, middleware will execute for all the routes;
+app.use(express.urlencoded({extended:true}));
 
-// app.use('/api',[logger,authorize]); //if path specified, middleware will execute for urls starting with this path
+app.post('/login',(req,res)=>{
+    console.log(req.body);
+    const {name} = req.body;
+    if(name){
+        res.status(200).send(`Hi ${name}, welcome to the authorized page.`);
+    }
+    res.status(401).send('Unauthorised page.');
 
-app.use(morgan('tiny'));
-
-app.get('/',(req,res)=>{
-    res.status(200).send('Home Page');
-})
-
-app.get('/about',(req,res)=>{
-    res.send('About page');
-})
-
-app.get('/api/testerA',(req,res)=>{
-    console.log(req.user);
-    res.send('Tester A');
-})
-
-app.get('/api/testerB',(req,res)=>{
-    res.send('Tester B');
 })
 
 
 app.listen(5000,()=>{
     console.log(`Server listening to PORT 5000`);
-})
+})      
