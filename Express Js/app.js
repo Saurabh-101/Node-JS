062d7fb4c4ec9c63c9ff62fdb011ec5beefddfe3
Saurabh-101 <saurabhgupta72dh3@gmail.com>
2026-07-28 22:09:21 +0530

    First api tested on postman

--- Express Js/app.js
+++ Express Js/app.js
@@ -3,6 +3,18 @@ const app = express();
 
 
 app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+app.post('/api/people',(req,res)=>{
+
+    const {name} = req.body;
+
+    if(!name){
+        return res.status(400).json({success:false,msg:'Please enter name.'})
+    }
+    res.status(200).json({success:true,msg:`Welcome ${name}`});
+
+})
 
 app.post('/login',(req,res)=>{
     console.log(req.body);
@@ -13,7 +25,9 @@ app.post('/login',(req,res)=>{
     res.status(401).send('Unauthorised page.');
 
 })
-
+app.get('/',(req,res)=>{
+    res.send('Home Page');
+})
 
 app.listen(5000,()=>{
     console.log(`Server listening to PORT 5000`);
