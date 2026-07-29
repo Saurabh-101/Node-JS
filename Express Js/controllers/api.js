7a7c163d6615609d4fe156bffb43a3b787afaac4
Saurabh-101 <saurabhgupta72dh3@gmail.com>
2026-07-29 23:02:51 +0530

    Covered Controllers

---
+++ Express Js/controllers/api.js
@@ -0,0 +1,51 @@
+const {companySites} = require('../data'); 
+const getPeople = (req,res)=>{
+
+    const id = req.params.id;
+    const { desc } = req.body;
+    const company = companySites.find((companySite)=> companySite.id == id);
+
+    if(!company){
+        return res.status(404).json({success:false,msg:`Company with id ${id} not found`});
+    }
+
+
+    return res.status(200).json({success:true,companyDetails:company});
+
+
+};
+
+const putPeople = (req,res)=>{
+
+    const id = req.params.id;
+    const { desc } = req.body;
+    const company = companySites.find((companySite)=> companySite.id == id);
+
+    if(!company){
+        return res.status(404).json({success:false,msg:`Company with id ${id} not found`});
+    }
+
+    const newCompany = companySites.map((companySite)=>{
+        if(companySite.id==id){
+            companySite.description = desc;
+        }
+        return companySite;
+    })
+
+    return res.status(200).json({success:true,companyDetails:newCompany});
+
+
+};
+
+const createPeople = (req,res)=>{
+
+    const {name} = req.body;
+
+    if(!name){
+        return res.status(400).json({success:false,msg:'Please enter name.'})
+    }
+    res.status(200).json({success:true,msg:`Welcome ${name}`});
+
+};
+
+module.exports = {getPeople,createPeople,putPeople};
